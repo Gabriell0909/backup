e4a66e42a2277e9a95dd93c4a2783c3dc4bf0734
Gabriel <144287458+Gabriell0909@users.noreply.github.com>
2025-05-20 00:06:29 -0300

    Adição do codigo da tela de categorias

--- src/screens/Categorias/Categorias.jsx
+++ src/screens/Categorias/Categorias.jsx
@@ -1,9 +1,40 @@
-import { Text } from 'react-native';
+import { KeyboardAvoidingView, Platform, StatusBar, Text, View } from 'react-native';
+import { Ionicons} from '@expo/vector-icons'
+
+import { styles } from './Categorias.style';
+import Card from '../../components/card';
+import Input from '../../components/inputs';
+import ButtonC from '../../components/customButton';
 
 export default function Categorias() {
    return (
-      <>
-         <Text>Olá</Text>
-      </>
+      <KeyboardAvoidingView
+         style={{ flex: 1, alignItems: 'center' }}
+         behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+         keyboardVerticalOffset={Platform.OS === 'ios' ? 100 : 0}
+      >
+         <StatusBar translucent backgroundColor="transparent" />
+         <Card style={styles.banner}>
+            <Text style={styles.title}>Categorias</Text>
+         </Card>
+
+         <Input style={styles.inputs} placeholder='Digite um nome para a categoria'/>
+
+         <View style = {styles.containerButton}>
+            <ButtonC style={styles.buttonC}>
+               <Ionicons name='person-circle-outline' size={32}/>
+               <Text>Icone</Text>
+            </ButtonC>
+         </View>
+
+         <ButtonC style={styles.btn}>
+            <Text>Salvar</Text>
+         </ButtonC>
+
+         <View style = {{marginTop:70}}>
+            <Text>Categorias</Text>
+            
+         </View>
+      </KeyboardAvoidingView>
    );
 }
---
+++ src/screens/Categorias/Categorias.style.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+   banner: {
+      transform: [{ rotate: '-3deg' }],
+      marginTop: -25,
+      minHeight: 170,
+      width: '105%',
+      right: 5,
+      borderRadius: 25,
+      justifyContent: 'center',
+   },
+
+   title: {
+      transform: [{ rotate: '3deg' }],
+      textAlign: 'center',
+      fontSize: 25,
+      color: '#fff',
+      marginTop: 30,
+   },
+
+   inputs: {
+      width: '90%',
+      paddingStart:10,
+      marginTop:30
+   },
+
+   buttonC: {
+      alignItems: 'center',
+      justifyContent:'center',
+      width: '25%',
+      gap:5,
+      flexDirection:'row',
+   },
+
+   btn:{
+    marginTop:10,
+   },
+
+   containerButton: {
+      width: '100%',
+      alignItems: 'flex-start',
+      paddingStart: 19,
+      bottom: 30,
+   },
+});
